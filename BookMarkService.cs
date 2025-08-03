8e51e0472e7d38405f23526ac25953acc8bab391
thesherif90@gmail.com <thesherif90@gmail.com>
2025-08-03 08:54:00 -0400

    added some update

--- BookMarkService.cs
+++ BookMarkService.cs
@@ -4,30 +4,30 @@ namespace bookmarkr;
 
 public class BookMarkService
 {
-    private readonly List<Bookmark? _bookmarks = new();
+    private readonly List<Bookmark> _bookmarks = new();
 
     public void AddLink(string name, string url)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
-            Helper.ShowErrorMessage(["the `name` for the link is not provided. The expected sytnax is:", "bookmarkr link add <name> <url>"]);
+            // Helper.ShowErrorMessage(["the `name` for the link is not provided. The expected sytnax is:", "bookmarkr link add <name> <url>"]);
             return;
         }
 
         if (string.IsNullOrWhiteSpace(url))
         {
-            Helper.ShowErrorMessage(["the `url` for the link is not provided. The expected sytnax is:", "bookmarkr link add <name> <url>"]);
+            // Helper.ShowErrorMessage(["the `url` for the link is not provided. The expected sytnax is:", "bookmarkr link add <name> <url>"]);
             return;
         }
-        if (_bookmarks.Any(b => b.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+        if (_bookmarks.Any(b => b.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
         {
-            Helper.ShowErrorMessage(["A link with the name `{name}` already exists. It will thus not be added",
-            $"To update the existing link, use the command: bookmarkr link update `{name}` `{url}`"]);
+            // Helper.ShowErrorMessage(["A link with the name `{name}` already exists. It will thus not be added",
+            // $"To update the existing link, use the command: bookmarkr link update `{name}` `{url}`"]);
             return;
         }
 
-        _bookmarks.Add(new Bookmark {Name = name, Url = url});
-        Helper.ShowSuccessMessage(["Bookmark successfully added!"]);
+        _bookmarks.Add(new Bookmark { Name = name, Url = url });
+        // Helper.ShowSuccessMessage(["Bookmark successfully added!"]);
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Parsing;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 
 namespace bookmarkr;
 
 class Program
 {
-    static void Main(string[] args)
+    private BookMarkService _service = new BookMarkService();
+
+    static int Main(string[] args)
     {
+
+
         RootCommand rootCommand = new RootCommand("Bookmarkr is a bookmark manager provided as a CLI application");
 
-        rootCommand.Set;
+        rootCommand.SetAction(parseResult =>
+        {
+            OnHandleRootCommand();
+            return 0;
+        });
 
-        var parser = new CommandLineBuilder(rootCommand)
-            .UseDefaults()
-            .Build();
+        var linkCommand = new Command("link", "Manage bookmarks links");
+
+        rootCommand.Add(linkCommand);
+
+        var nameOption = new Option<string>("name", ["--name", "-n"]);
+        var urlOption = new Option<string>("url", ["--url", "-u"]);
+
+        var addLinkCommand = new Command("add", "Add a new bookmark link");
+        addLinkCommand.Add(nameOption);
+        addLinkCommand.Add(urlOption);
+
+        linkCommand.Add(addLinkCommand);
+
+        addLinkCommand.SetAction(parseResult =>
+        {
+            var name = parseResult.GetValue<string>("name");
+            var url = parseResult.GetValue<string>("url");
+
+            OnHandleAddLinkCommand(name, url);
+            return 0;
+        });
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
     }
 
     private static void OnHandleRootCommand()
     {
         Console.WriteLine("Hello from the root command!");
+    }
+
+    private static void OnHandleAddLinkCommand(string name, string url)
+    {
+        Console.WriteLine("hello again");
+        var service = new BookMarkService();
+        service.AddLink(name, url);
     }
 }
